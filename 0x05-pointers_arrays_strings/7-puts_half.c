60f2ecbdaf2021a191283441f3ca4d02e9f58bd4
K1ll-Sw1tch <flowprince156@gmail.com>
2023-08-21 16:07:35 -0700

    Task 7

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -12,14 +12,14 @@ void puts_half(char *str)
 	int full_str, half_str;
 
 	full_str = 0;
-	while (str[full_str] != ‘\0’)
+	while (str[full_str] != '\0')
 		full_str++;
 	half_str = full_str / 2;
 	if (full_str % 2 == 1)
 		half_str++;
 	while (half_str < full_str)
 	{
-		_putchar(str[half_string]);
+		_putchar(str[half_str]);
 		half_str++;
 	}
 	_putchar('\n');
