a6c8642aeb673c622522733b56eb73c5f9ed08a9
K1ll-Sw1tch <flowprince156@gmail.com>
2023-08-25 13:01:37 -0700

    Task 9

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_number - prints an integer
+ * @n: integer parameter
+ *@un : unsigned integer parameter
+ * Return: 0
+ */
+
+void print_number(int n)
+{
+	unsigned int un;
+
+	un = n;
+	if (n < 0)
+	{
+		_putchar('-');
+		un = -n;
+	}
+	if (un / 10 != 0)
+	{
+		print_number(un / 10);
+	}
+	_putchar((un % 10) + '0');
+}
